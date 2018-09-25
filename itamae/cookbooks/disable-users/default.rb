7bf3b62ee886ccbd1b417e57c8b583e90c315a2a
Kugayama Nana <nonamea774@gmail.com>
2018-09-26 02:15:25 +0900

    add disable users cookbook

---
+++ itamae/cookbooks/disable-users/default.rb
@@ -0,0 +1,6 @@
+(node.dig(:disable_user, :names) || []).each do |username|
+  execute 'lock user' do
+    command "passwd -l #{username}"
+    not_if "grep -E '^#{username}:!' /etc/shadow"
+  end
+end

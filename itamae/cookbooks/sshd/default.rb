2f7cebceb58b7e8d0f531a40cae3ae6d581ec1dd
Kugayama Nana <nonamea774@gmail.com>
2018-09-26 03:22:42 +0900

    sshd settings

--- itamae/cookbooks/sshd/default.rb
+++ itamae/cookbooks/sshd/default.rb
@@ -5,12 +5,18 @@ unless allow_users.nil?
   allow_users.each { |u| allow_users_c += " #{u}" }
 end
 
+sftp_path = '/usr/lib/ssh/sftp-server'
+if node[:os] == :ubuntu
+  sftp_path = '/usr/lib/openssh/sftp-server'
+end
+
 template '/etc/ssh/sshd_config' do
   owner 'root'
   group 'root'
   mode '0644'
   variables(
-    allow_users: allow_users_c
+    allow_users: allow_users_c,
+    sftp_path: sftp_path,
   )
   notifies :restart, 'service[sshd]'
 end

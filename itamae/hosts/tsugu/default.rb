b4ae57d84c7c26787a68fd143c100ab66796cfda
Kugayama Nana <nonamea774@gmail.com>
2018-09-27 14:04:30 +0900

    tsugu network settings

--- itamae/hosts/tsugu/default.rb
+++ itamae/hosts/tsugu/default.rb
@@ -21,3 +21,14 @@ include_cookbook 'sshd'
 include_cookbook 'hostname'
 include_cookbook 'cloud-init'
 include_cookbook 'strongswan'
+
+%w(
+/etc/sysctl.d/20-ipv4.ip_forward.conf
+/etc/systemd/network/15-ens6.network
+).each do |f|
+  remote_file f do
+    owner 'root'
+    group 'root'
+    mode '0644'
+  end
+end

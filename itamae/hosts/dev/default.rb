1855591ecdc436710c59a9fb0e517e3f1b3c9ed0
Kugayama Nana <nonamea774@gmail.com>
2018-10-31 18:34:44 +0900

    dev向けにgolangを入れます。パッケージで十分新しいでしょう。

--- itamae/hosts/dev/default.rb
+++ itamae/hosts/dev/default.rb
@@ -1,4 +1,4 @@
-packages = %w(git htop tmux emacs-nox make build-essential mongodb-clients)
+packages = %w(git htop tmux emacs-nox make build-essential mongodb-clients golang-go)
 packages.each do |p|
   package p do
     action :install
@@ -30,4 +30,11 @@ package 'docker-ce'
 execute 'install docker-compose' do
   command 'curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose; chmod +x /usr/local/bin/docker-compose'
   not_if 'test -e /usr/local/bin/docker-compose'
+end
+
+# go
+gopath = '$HOME/.go'
+execute 'set gopath' do
+  command "mkdir -p #{gopath}; touch ~/.bashrc; echo GOPATH=#{gopath} >> ~/.bashrc"
+  not_if 'grep \'GOPATH=\' ~/.bashrc'
 end

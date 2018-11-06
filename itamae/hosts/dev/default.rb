c0a7909dc8fd89a2dfd2c3bd5611505fc2dc7fdb
Kugayama Nana <nonamea774@gmail.com>
2018-11-06 14:55:32 +0900

    add traceroute to dev

--- itamae/hosts/dev/default.rb
+++ itamae/hosts/dev/default.rb
@@ -1,4 +1,4 @@
-packages = %w(git htop tmux emacs-nox make build-essential mongodb-clients golang-go)
+packages = %w(git htop tmux emacs-nox make build-essential mongodb-clients golang-go traceroute)
 packages.each do |p|
   package p do
     action :install

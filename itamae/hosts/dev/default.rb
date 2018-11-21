a3692dce9d04bc778ba8995921e4127990bebd06
Kugayama Nana <nonamea774@gmail.com>
2018-11-21 15:58:54 +0900

    jqをdevに入れる。

--- itamae/hosts/dev/default.rb
+++ itamae/hosts/dev/default.rb
@@ -1,4 +1,4 @@
-packages = %w(git htop tmux emacs-nox make build-essential mongodb-clients golang-go traceroute nmap)
+packages = %w(git htop tmux emacs-nox make build-essential mongodb-clients golang-go traceroute nmap jq)
 packages.each do |p|
   package p do
     action :install

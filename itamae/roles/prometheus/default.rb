aefefd8c537cf0cc7fb9153b13d1f5c168149093
Kugayama Nana <nonamea774@gmail.com>
2019-03-15 18:21:18 +0900

    install grafana

--- itamae/roles/prometheus/default.rb
+++ itamae/roles/prometheus/default.rb
@@ -21,3 +21,12 @@ template '/etc/prometheus/alertmanager.yml' do
   mode '0644'
   notifies :restart, 'service[prometheus-alertmanager]'
 end
+
+execute 'install grafana' do
+  command 'wget https://dl.grafana.com/oss/release/grafana_6.0.1_amd64.deb -O /tmp/grafana.deb && dpkg -i /tmp/grafana.deb'
+  not_if 'apt show grafana | grep installed'
+end
+
+service 'grafana-server' do
+  action [:start, :enable]
+end

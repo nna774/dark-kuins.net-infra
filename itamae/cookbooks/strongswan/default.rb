ed6425a56b7cb2483586217f81724aa8ff4488ce
Kugayama Nana <nonamea774@gmail.com>
2019-07-07 04:33:07 +0900

    debian fix

--- itamae/cookbooks/strongswan/default.rb
+++ itamae/cookbooks/strongswan/default.rb
@@ -1,6 +1,22 @@
 package 'strongswan-swanctl'
 package 'charon-systemd'
 
+if node[:os] == :debian && node[:release] == :stretch
+  # for libstrongswan-updown.so
+  package 'strongswan-starter'
+
+  link '/etc/systemd/system/strongswan.service' do
+    to '/dev/null'
+    notifies :run, 'execute[systemctl daemon-reload]'
+  end
+end
+
+directory '/etc/swanctl/conf.d' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
 template '/etc/swanctl/conf.d/yukari.conf' do
   owner 'root'
   group 'root'

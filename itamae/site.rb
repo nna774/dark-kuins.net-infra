91181d40a51ee1f9658dc8156d0d7cbedf701453
Kugayama Nana <nonamea.774@gmail.com>
2022-08-31 17:24:23 +0900

    bunki

--- itamae/site.rb
+++ itamae/site.rb
@@ -1,5 +1,11 @@
 node[:basedir] = File.expand_path('..', __FILE__)
-node[:secrets] = MitamaeSecrets::Store.new(File.join(node[:basedir],'secrets'))
+node[:mitamae] = Object.const_defined?(:MItamae)
+
+if node[:mitamae]
+  include_recipe './site_mitamae.rb'
+else
+  include_recipe './site_itamae.rb'
+end
 
 execute 'systemctl daemon-reload' do
   action :nothing
@@ -37,35 +43,4 @@ when 'debian'
   node[:is_systemd] = node[:release] != '7'
 when 'arch'
   node[:is_systemd] = true
-end
-
-MItamae::RecipeContext.class_eval do
-  ROLES_DIR = File.expand_path("../roles", __FILE__)
-  def include_role(name)
-    recipe_file = File.join(ROLES_DIR, name, "default.rb")
-    include_recipe(recipe_file.to_s)
-  end
-
-  COOKBOOKS_DIR = File.expand_path("../cookbooks", __FILE__)
-  def include_cookbook(name)
-    names = name.split("::")
-    names << "default" if names.length == 1
-    names[-1] += ".rb"
-
-    candidates = [
-      File.join(COOKBOOKS_DIR, *names),
-    ]
-    candidates.each do |candidate|
-      if File.exist?(candidate)
-        include_recipe(candidate)
-        return
-      end
-    end
-    raise "Cookbook #{name} couldn't found"
-  end
-
-  DATA_BAGSS_DIR = File.expand_path('../data_bags', __FILE__)
-  def data_bag(name)
-    YAML.load(File.read(File.join(DATA_BAGSS_DIR, "#{name}.yml")))
-  end
 end
---
+++ itamae/site_itamae.rb
@@ -0,0 +1,41 @@
+require 'itamae/secrets'
+require 'yaml'
+require 'pathname'
+
+node[:secrets] = Itamae::Secrets(File.join(__dir__, 'secrets'))
+
+module RecipeHelper
+  def include_role(name)
+    Pathname.new(File.dirname(@recipe.path)).ascend do |dir|
+      recipe_file = dir.join("roles", name, "default.rb")
+      if recipe_file.exist?
+        include_recipe(recipe_file.to_s)
+        return
+      end
+    end
+
+    raise "Role #{name} is not found."
+  end
+
+  def include_cookbook(name)
+    names = name.split("::")
+    names << "default" if names.length == 1
+    names[-1] += ".rb"
+
+    Pathname.new(File.dirname(@recipe.path)).ascend do |dir|
+      recipe_file = dir.join("cookbooks", *names)
+      if recipe_file.exist?
+        include_recipe(recipe_file.to_s)
+        return
+      end
+    end
+
+    raise "Cookbook #{name} is not found."
+  end
+
+  def data_bag(name)
+    Hashie::Mash.load(File.join(__dir__, 'data_bags', "#{name}.yaml"))
+  end
+end
+
+Itamae::Recipe::EvalContext.send(:include, RecipeHelper)

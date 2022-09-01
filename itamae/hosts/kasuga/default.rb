70496fd1a75d53e9b478dd5292259f50545af2f0
Kugayama Nana <nonamea.774@gmail.com>
2022-09-01 22:30:12 +0900

    sudo ga okoru

--- itamae/hosts/kasuga/default.rb
+++ itamae/hosts/kasuga/default.rb
@@ -15,6 +15,26 @@ node.reverse_merge!({
     interfacesv4: 'ens4',
   },
 })
+
+file '/etc/hosts' do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  content <<~EOS
+    127.0.0.1 localhost
+    127.0.0.1 kasuga.compute.nakanoshima.dark-kuins.net kasuga
+
+    # The following lines are desirable for IPv6 capable hosts
+    ::1 ip6-localhost ip6-loopback
+    fe00::0 ip6-localnet
+    ff00::0 ip6-mcastprefix
+    ff02::1 ip6-allnodes
+    ff02::2 ip6-allrouters
+    ff02::3 ip6-allhosts
+    169.254.169.254 metadata.google.internal metadata
+  EOS
+end
+
 include_cookbook 'strongswan'
 
 include_cookbook 'slack'

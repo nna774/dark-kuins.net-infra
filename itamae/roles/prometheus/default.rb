e81252b79d47b00ec45a92efb46633ae33261dc1
Kugayama Nana <nonamea.774@gmail.com>
2023-05-30 19:05:43 +0900

    add ARGS

--- itamae/roles/prometheus/default.rb
+++ itamae/roles/prometheus/default.rb
@@ -8,6 +8,13 @@ packages.each do |p|
   end
 end
 
+template '/etc/default/prometheus' do
+  owner 'prometheus'
+  group 'prometheus'
+  mode '0644'
+  notifies :restart, 'service[prometheus]'
+end
+
 template '/etc/prometheus/prometheus.yml' do
   owner 'prometheus'
   group 'prometheus'
@@ -22,6 +29,7 @@ template '/etc/prometheus/alertmanager.yml' do
   notifies :restart, 'service[prometheus-alertmanager]'
 end
 
+=begin
 execute 'install grafana' do
   command 'wget https://dl.grafana.com/oss/release/grafana_6.0.1_amd64.deb -O /tmp/grafana.deb && dpkg -i /tmp/grafana.deb'
   not_if 'apt show grafana | grep installed'
@@ -30,3 +38,4 @@ end
 service 'grafana-server' do
   action [:start, :enable]
 end
+=end

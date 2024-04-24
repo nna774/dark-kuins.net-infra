626fbf914d7c03b67cdfb1d988736d32eda7c1b9
Kugayama Nana <nonamea.774@gmail.com>
2024-04-25 06:50:20 +0900

    ない場合もOK

--- itamae/cookbooks/disable-users/default.rb
+++ itamae/cookbooks/disable-users/default.rb
@@ -1,6 +1,6 @@
 (node.dig(:disable_user, :names) || []).each do |username|
   execute 'lock user' do
     command "passwd -l #{username}"
-    not_if "grep -E '^#{username}:!' /etc/shadow"
+    not_if "grep -E '^#{username}:!' /etc/shadow || grep -v -E '^#{username}:' /etc/shadow"
   end
 end
